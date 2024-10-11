9cb9079f1f126f16272522c11155803f99e88cb9
jjtp0245 <154034604+jjtp0245@users.noreply.github.com>
2024-10-11 12:45:04 +0700

    Update button label in MyCard component

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,8 +26,8 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-    <Button size="small">Learn More</Button>
-    <Button size="small">test v4</Button>
+    <Button size="small">Lear1 More</Button>
+    <Button size="small">test v2</Button>
   </CardActions>
 </Card>
 
