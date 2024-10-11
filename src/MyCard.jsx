4043cc3d5b218018e32b041f964c5ad1d90bc2ff
jjtp0245 <154034604+jjtp0245@users.noreply.github.com>
2024-10-11 12:30:19 +0700

    test workflow v4

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -27,6 +27,7 @@ export default function MyCard(props) {
   </CardContent>
   <CardActions>
     <Button size="small">Learn More</Button>
+    <Button size="small">test v4</Button>
   </CardActions>
 </Card>
 
